ecf9610e288d509505f42da9918b3ebf9878179c
Stefan <stefan@furne.net>
2022-02-23 21:36:54 +0100

    New battery widget, based on qtile-extras

--- battery.py
+++ battery.py
@@ -3,65 +3,115 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import psutil  # type: ignore
+import asyncio
+
+from dbus_next.aio import MessageBus
+from dbus_next.constants import BusType
 
 from libqtile.widget import base
-from libqtile.log_utils import logger
 
 from colors import colors
 
 if TYPE_CHECKING:
-    from typing import Dict
+    from typing import Dict, Any
 
 
-class CustomBattery(base.ThreadPoolText):
+BATTERY = "/org/freedesktop/UPower/devices/battery_BAT0"
+PROPS_IFACE = "org.freedesktop.DBus.Properties"
+UPOWER_SERVICE = "org.freedesktop.UPower"
+UPOWER_INTERFACE = "org.freedesktop.UPower"
+UPOWER_PATH = "/org/freedesktop/UPower"
+UPOWER_DEVICE = UPOWER_INTERFACE + ".Device"
+UPOWER_BUS = BusType.SYSTEM
+
+battery_level_icons: Dict[str, int] = {
+    "": 95,
+    "": 90,
+    "": 80,
+    "": 70,
+    "": 60,
+    "": 50,
+    "": 40,
+    "": 30,
+    "": 20,
+    "": 10,
+    "": 0,
+}
+
+
+class CustomBattery(base._TextBox):
     """Displaying a battery icon and percentage"""
 
-    orientations = base.ORIENTATION_HORIZONTAL
-    defaults = [("update_interval", 15, "Update time in seconds.")]
-
     def __init__(self, **config):
-        base.ThreadPoolText.__init__(self, "", **config)
-        self.add_defaults(CustomBattery.defaults)
+        base._TextBox.__init__(self, **config)
 
-        self.battery_level_icons: Dict[str, int] = {
-            "": 95,
-            "": 90,
-            "": 80,
-            "": 70,
-            "": 60,
-            "": 50,
-            "": 40,
-            "": 30,
-            "": 20,
-            "": 10,
-            "": 0,
-        }
+        self.battery_status = None
+        self.battery_dev = None
+        self.upower = None
+        self.bus = None
+        self.charging: bool = False
 
-        self.text = self.poll()
+    async def _config_async(self):
+        await self._setup_dbus()
 
-    def poll(self) -> str:
-        """Get the battery level, return the corresponding icon"""
+    async def _setup_dbus(self):
+        # Set up connection to DBus
+        self.bus = await MessageBus(bus_type=UPOWER_BUS).connect()
+        introspection = await self.bus.introspect(UPOWER_SERVICE, UPOWER_PATH)
+        proxy_object = self.bus.get_proxy_object(
+            UPOWER_SERVICE, UPOWER_PATH, introspection
+        )
 
-        battery = psutil.sensors_battery()
+        props = proxy_object.get_interface("org.freedesktop.DBus.Properties")
+        props.on_properties_changed(self.upower_change)
 
-        if battery.power_plugged is False:
-            battery_icon = next(
-                iter(
-                    {
-                        k: v
-                        for k, v in self.battery_level_icons.items()
-                        if battery.percent >= v
-                    }
-                )
-            )
-        elif battery.power_plugged is True and battery.percent == 100:
-            battery_icon = ""
+        self.upower = proxy_object.get_interface(UPOWER_INTERFACE)
 
-        elif battery.power_plugged is True:
+        # Get battery details from DBus
+        self.battery_status: Dict[str, Any] = await self.get_battery()
+
+        # Is laptop charging?
+        self.charging: bool = not await self.upower.get_on_battery()
+
+        self.configured = await self._update_battery_info()
+
+    async def get_battery(self):
+        """Get the device and fetch its info"""
+
+        introspection = await self.bus.introspect(UPOWER_SERVICE, BATTERY)
+        battery_obj = self.bus.get_proxy_object(UPOWER_SERVICE, BATTERY, introspection)
+        self.battery_dev = battery_obj.get_interface(UPOWER_DEVICE)
+        props = battery_obj.get_interface(PROPS_IFACE)
+
+        # Listen for change signals on DBus
+        props.on_properties_changed(self.battery_change)
+
+        await self._update_battery_info()
+
+    def upower_change(self, interface, changed, invalidated):
+        """Update the charging status"""
+        del interface, changed, invalidated
+        asyncio.create_task(self._upower_change())
+
+    async def _upower_change(self):
+        self.charging = not await self.upower.get_on_battery()
+        asyncio.create_task(self._update_battery_info())
+
+    def battery_change(self, interface, changed, invalidated):
+        """The batteries are polled every 2 mins by DBus"""
+        del interface, changed, invalidated
+        asyncio.create_task(self._update_battery_info())
+
+    async def _update_battery_info(self):
+        percentage = await self.battery_dev.get_percentage()
+        if self.charging:
             battery_icon = ""
-
+        elif self.charging and percentage == 100:
+            battery_icon = ""
         else:
-            logger.error("Cannot determine battery status.")
+            battery_icon = next(
+                iter({k: v for k, v in battery_level_icons.items() if percentage >= v})
+            )
 
-        return f"{battery_icon} <span foreground='{colors['text']}'>{round(battery.percent)}%</span>"
+        self.qtile.call_soon(self.bar.draw)
+        self.text = f"{battery_icon} <span foreground='{colors['text']}'>{round(percentage)}%</span>"
