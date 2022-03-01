c35bc22c9446f6382494f959fd42162068f56e5a
Stefan <stefan@furne.net>
2022-03-01 22:16:35 +0100

    Mypy adjustments

--- wifi.py
+++ wifi.py
@@ -14,7 +14,10 @@ from libqtile.widget import base
 from colors import colors
 
 if TYPE_CHECKING:
-    from typing import Dict, Any
+    from typing import Callable, Dict, List, Any, Union
+    from dbus_next.signature import Variant
+    from dbus_next.aio.proxy_object import ProxyInterface, ProxyObject
+    from dbus_next.introspection import Node
 
 
 CONNMAN_SERVICE = "net.connman"
@@ -26,38 +29,39 @@ CONNMAN_BUS = BusType.SYSTEM
 class Wifi(base._TextBox):
     """Displaying a battery icon and percentage"""
 
-    def __init__(self, **config):
+    def __init__(self, **config) -> None:
         base._TextBox.__init__(self, **config)
-        self.bus = None
-        self.connman = None
+        self.bus: MessageBus
+        self.connman: ProxyInterface
 
-    async def _config_async(self):
+    async def _config_async(self) -> None:
         await self._setup_dbus()
 
-    async def _setup_dbus(self):
+    async def _setup_dbus(self) -> None:
         # Set up connection to DBus
         self.bus = await MessageBus(bus_type=CONNMAN_BUS).connect()
-        introspection = await self.bus.introspect(CONNMAN_SERVICE, CONNMAN_PATH)
-        proxy_object = self.bus.get_proxy_object(
+        introspection: Node = await self.bus.introspect(CONNMAN_SERVICE, CONNMAN_PATH)
+        proxy_object: ProxyObject = self.bus.get_proxy_object(
             CONNMAN_SERVICE, CONNMAN_PATH, introspection
         )
 
         self.connman = proxy_object.get_interface(CONNMAN_INTERFACE)
-        self.connman.on_property_changed(self.connman_change)
+        self.connman.on_property_changed(self.connman_change)  # type: ignore
 
         self.configured = await self._update_wifi_info()
 
-    def connman_change(self, interface, changed):
-        """Update the charging status"""
+    def connman_change(self, interface, changed) -> None:
+        """Listen to wifi changes"""
         del interface, changed
         asyncio.create_task(self._update_wifi_info())
 
-    async def _update_wifi_info(self):
-        try:
-            wifi_info = await self.connman.call_get_services()
-            status = wifi_info[0][1]["State"].value
-        except IndexError:
+    async def _update_wifi_info(self) -> None:
+        """Update the info in the widget"""
+        wifi_info: List[List[Dict[str, Variant]]] = await self.connman.call_get_services()  # type: ignore
+        if not wifi_info:
             status = None
+        else:
+            status = wifi_info[0][1]["State"].value
 
         if status == "online":
             wifi_icon = "直"
