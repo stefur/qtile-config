601328c7318f86f79577e9292054a88f77764c84
Stefan <stefan@furne.net>
2022-03-03 22:47:51 +0100

    Rewrite while learning DBus

--- spotify.py
+++ spotify.py
@@ -1,123 +1,147 @@
-"""A custom widget to show what Spotify is playing, based on mpris2 widget. Requires Nerd fonts."""
+"""A custom widget to show what Spotify is playing. Requires Nerd fonts."""
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-from dbus_next.constants import MessageType  # type: ignore
 
-from libqtile.log_utils import logger
-from libqtile.utils import add_signal_receiver
+import asyncio
+
+from dbus_next import Message
+from dbus_next.aio import MessageBus
+from dbus_next.constants import BusType, MessageType
+
 from libqtile.widget import base
+from libqtile.utils import logger
+
 from colors import colors
 
 if TYPE_CHECKING:
-    from typing import Any, Dict, List
-    from dbus_next.message import Message
+    from typing import Dict, Any, Union, List, Optional
+    from dbus_next.signature import Variant
+    from dbus_next.aio.proxy_object import ProxyInterface, ProxyObject
+    from dbus_next.introspection import Node
+
+SPOTIFY_SERVICE = "org.mpris.MediaPlayer2.spotify"
+SPOTIFY_INTERFACE = "org.freedesktop.DBus.Properties"
+SPOTIFY_PATH = "/org/mpris/MediaPlayer2"
+DBUS_PATH = "/org/freedesktop/DBus"
+DBUS_INTERFACE = "org.freedesktop.DBus"
+SESSION_BUS = BusType.SESSION
 
 
 class NowPlaying(base._TextBox):
     """Basically set up a listener for Spotify according to my liking"""
 
-    def __init__(self, **config):
+    def __init__(self, **config) -> None:
         base._TextBox.__init__(self, **config)
 
-        self.statusicon = ""
-        self.displaytext = ""
-        self.display_metadata = ["xesam:artist", "xesam:title"]
+        self.bus: MessageBus
+        self.messagebody: List[Any] = []
+        self.now_playing: Optional[str] = None
+        self.playback_icon: Optional[str] = None
+        self.properties_changed: Optional[Message] = None
+        self.name_owner_changed: Optional[Message] = None
 
-    async def _config_async(self):
+    async def _config_async(self) -> None:
+        await self._setup_dbus()
 
-        subscribe_update = await add_signal_receiver(
-            self.updatemessage,
-            session_bus=True,
-            signal_name="PropertiesChanged",
-            bus_name="org.mpris.MediaPlayer2.spotify",
-            path="/org/mpris/MediaPlayer2",
-            dbus_interface="org.freedesktop.DBus.Properties",
+    async def _setup_dbus(self) -> None:
+        self.bus = await MessageBus(bus_type=SESSION_BUS).connect()
+
+        self.properties_changed = await self.bus.call(
+            Message(
+                message_type=MessageType.METHOD_CALL,
+                destination="org.freedesktop.DBus",
+                interface="org.freedesktop.DBus",
+                path="/org/freedesktop/DBus",
+                member="AddMatch",
+                signature="s",
+                body=[
+                    f"type='signal',sender='{SPOTIFY_SERVICE}',member='PropertiesChanged',path='{SPOTIFY_PATH}',interface='{SPOTIFY_INTERFACE}'"
+                ],
+            )
         )
 
-        if not subscribe_update:
-            msg = "Unable to add signal receiver for Spotify."
-            logger.warning(msg)
-
-        subscribe_closed = await add_signal_receiver(
-            self.closemessage,
-            session_bus=True,
-            signal_name="NameOwnerChanged",
-            path="/org/freedesktop/DBus",
-            dbus_interface="org.freedesktop.DBus",
+        self.name_owner_changed = await self.bus.call(
+            Message(
+                message_type=MessageType.METHOD_CALL,
+                destination="org.freedesktop.DBus",
+                interface="org.freedesktop.DBus",
+                path="/org/freedesktop/DBus",
+                member="AddMatch",
+                signature="s",
+                body=[
+                    f"type='signal',sender='{DBUS_INTERFACE}',member='NameOwnerChanged',path='{DBUS_PATH}',interface='{DBUS_INTERFACE}'"
+                ],
+            )
         )
 
-        if not subscribe_closed:
-            msg = "Unable to add signal receiver for when Spotify is closed."
-            logger.warning(msg)
+        if not self.properties_changed or not self.name_owner_changed:
+            logger.error("Failed to send messages with AddMatch to DBus")
 
-    def updatemessage(self, updatemessage: Message) -> None:
+        self.bus.add_message_handler(self.message_handler)
+
+    def message_handler(self, updatemessage: Message) -> None:
         """Send the properties if an update is received, e.g. new song or playback status"""
 
+        # Only listen to signal messages
         if updatemessage.message_type != MessageType.SIGNAL:
             return
 
-        self.update_info(*updatemessage.body)
-
-    def closemessage(self, closemessage: Message) -> None:
-        """Send the message that Spotify has been closed"""
-        if closemessage.message_type != MessageType.SIGNAL:
+        # Attempt to skip repeated signals
+        if updatemessage.body == self.messagebody:
             return
 
-        self.closed(*closemessage.body)
+        if updatemessage.member == "PropertiesChanged":
+            self.spotify_changed(*updatemessage.body)
+            self.messagebody = updatemessage.body
 
-    def closed(self, name: str, old_owner: str, new_owner: str) -> None:
-        """If Spotify is closed, clear the text in the widget"""
+        if (
+            updatemessage.member == "NameOwnerChanged"
+            and SPOTIFY_SERVICE in updatemessage.body[0]
+        ):
+            self.spotify_nameowner(*updatemessage.body)
+            self.messagebody = updatemessage.body
 
-        del new_owner
-        if name == "org.mpris.MediaPlayer2.spotify" and old_owner:
-            self.text = ""
-            self.bar.draw()
+    def spotify_changed(
+        self, interface: str, changed: Dict[str, Variant], invalidated: List[Any]
+    ) -> None:
+        """Send the properties if an update is received, e.g. new song or playback status"""
+        del interface, invalidated
+        metadata = changed.get("Metadata")
+        metadata = metadata.value  # type: ignore
+        playbackstatus = changed.get("PlaybackStatus")
+        playbackstatus = playbackstatus.value  # type: ignore
+        asyncio.create_task(self.update_widget(metadata, playbackstatus))
 
-    def update_info(
-        self,
-        interface_name: str,
-        changed_properties: Dict[str, Any],
-        _invalidated_properties: List[Any],
+    def spotify_nameowner(self, name: str, old_owner: str, new_owner: str) -> None:
+        """If the nameowner for Spotify changed we assume it has closed and clear the text in the widget"""
+        del name, old_owner, new_owner
+        self.qtile.call_soon(self.bar.draw)
+        self.text = ""
+
+    async def update_widget(
+        self, metadata: Optional[Variant], playbackstatus: Optional[Variant]
     ) -> None:
         """Update song artist and title, including playback status"""
 
-        del interface_name
-        metadata = changed_properties.get("Metadata")
-        if metadata:
-            metadata = metadata.value
+        artist = metadata["xesam:artist"].value[0]  # type: ignore
+        song = metadata["xesam:title"].value  # type: ignore
+        self.now_playing = f"{artist} - {song}"
+        self.now_playing.replace("\n", "")
 
-            meta_list = []
-            for key in self.display_metadata:
-                val = getattr(metadata.get(key), "value", None)
-                if isinstance(val, str):
-                    meta_list.append(val)
-                elif isinstance(val, list):
-                    val = " - ".join((y for y in val if isinstance(y, str)))
-                    meta_list.append(val)
+        if len(self.now_playing) > 35:
+            self.now_playing = self.now_playing[:35]
+            self.now_playing += "…"
+            if "(" in self.now_playing and ")" not in self.now_playing:
+                self.now_playing += ")"
 
-            self.displaytext = " - ".join(meta_list)
-            self.displaytext.replace("\n", "")
-            if len(self.displaytext) > 35:
-                self.displaytext = self.displaytext[:35]
-                self.displaytext += "…"
-                if ("(" in self.displaytext) and (")" not in self.displaytext):
-                    self.displaytext += ")"
-
-        playbackstatus = getattr(
-            changed_properties.get("PlaybackStatus"), "value", None
-        )
         if playbackstatus == "Paused":
-            self.statusicon = f"<span foreground='{colors['primary']}'> \
+            self.playback_icon = f"<span foreground='{colors['primary']}'> \
                                  </span>"
 
         elif playbackstatus == "Playing":
-            self.statusicon = f"<span foreground='{colors['primary']}'> \
+            self.playback_icon = f"<span foreground='{colors['primary']}'> \
                                 契 </span>"
-        elif self.displaytext:
-            # Spotify usually send more than one "Playing" message.
-            pass
-
-        if self.text != self.displaytext:
-            self.text = self.statusicon + self.displaytext
-            self.bar.draw()
+        if self.text != self.now_playing:
+            self.qtile.call_soon(self.bar.draw)
+            self.text = f"{self.playback_icon} {self.now_playing}"
