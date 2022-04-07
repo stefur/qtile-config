e0a2d97e3a9f5e91efeebaf4dd70f5839c49cc5e
Stefan <stefan@furne.net>
2022-04-07 18:20:55 +0200

    Switched to lavender

--- colors.py
+++ colors.py
@@ -3,7 +3,7 @@ Defining colors, using catppuccin atm
 """
 
 colors = {
-    "primary": "#B5E8E0",  # Teal
+    "primary": "#C9CBFF",  # Lavender
     "secondary": "#6E6C7E",  # Gray 0
     "background": "#161320",  # Black 0
     "text": "#D9E0EE",  # White
