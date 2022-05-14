e3f3975c962508fe21559dfb360c63257776b8aa
Stefan <stefan@furne.net>
2022-05-14 09:04:52 +0200

    Split up the message handling

--- spotify.py
+++ spotify.py
@@ -81,69 +81,66 @@ class Spotify(widget.TextBox):
     def message_handler(self, updatemessage: Message) -> None:
         """Send the properties if an update is received, e.g. new song or playback status"""
 
-        if updatemessage.member == "PropertiesChanged":
-            self.spotify_changed(*updatemessage.body)
+        if (
+            updatemessage.member == "PropertiesChanged"
+            and list(updatemessage.body[1].keys())[0] == "Metadata"
+        ):
+            asyncio.create_task(self.metadata_changed(*updatemessage.body))
+
+        if (
+            updatemessage.member == "PropertiesChanged"
+            and list(updatemessage.body[1].keys())[0] == "PlaybackStatus"
+        ):
+            asyncio.create_task(self.playback_changed(*updatemessage.body))
 
         if (
             updatemessage.member == "NameOwnerChanged"
             and SPOTIFY_SERVICE in updatemessage.body[0]
         ):
-            self.spotify_nameowner(*updatemessage.body)
+            asyncio.create_task(self.spotify_nameowner(*updatemessage.body))
 
-    def spotify_changed(
+    async def metadata_changed(
         self, interface: str, changed: dict[str, Variant], invalidated: list[Any]
     ) -> None:
-        """Send the properties if an update is received, e.g. new song or playback status"""
+        """Update the song info in the widget"""
         del interface, invalidated  # Unused parameters
 
-        # TODO: Make message handling more elegant
-        try:
-            metadata = changed["Metadata"]
-        except Exception:
-            metadata = None
+        artist = changed["Metadata"].value["xesam:artist"].value[0]
+        song = changed["Metadata"].value["xesam:title"].value
+        self.now_playing = f"{artist} - {song}"
+        self.now_playing.replace("\n", "")
 
-        try:
-            playbackstatus = changed["PlaybackStatus"].value
-        except Exception:
-            playbackstatus = "Playing"  # Assume autoplay upon next song
+        if len(self.now_playing) > 35:
+            self.now_playing = self.now_playing[:35]
+            self.now_playing += "…"
+            if "(" in self.now_playing and ")" not in self.now_playing:
+                self.now_playing += ")"
 
-        asyncio.create_task(self.update_widget(playbackstatus, metadata))
+        self.now_playing = self.now_playing.replace("&", "&amp;")
 
-    def spotify_nameowner(self, name: str, old_owner: str, new_owner: str) -> None:
+        self.qtile.call_soon(self.bar.draw)
+        self.text = f"{self.playback_icon} {self.now_playing}"
+
+    async def playback_changed(
+        self, interface: str, changed: dict[str, Variant], invalidated: list[Any]
+    ) -> None:
+        """Update the playback icon in the widget"""
+        del interface, invalidated  # Unused parameters
+
+        if changed["PlaybackStatus"].value == "Paused":
+            self.playback_icon = f"<span foreground='{colors['primary']}'> \
+                                \uf8e3</span>"
+
+        elif changed["PlaybackStatus"].value == "Playing":
+            self.playback_icon = f"<span foreground='{colors['primary']}'> \
+                                \uf909</span>"
+        self.qtile.call_soon(self.bar.draw)
+        self.text = f"{self.playback_icon} {self.now_playing}"
+
+    async def spotify_nameowner(
+        self, name: str, old_owner: str, new_owner: str
+    ) -> None:
         """If the nameowner for Spotify changed we assume it has closed and clear the text in the widget"""
         del name, old_owner, new_owner  # Unused parameters
         self.qtile.call_soon(self.bar.draw)
         self.text = ""
-
-    async def update_widget(
-        self,
-        playbackstatus: str | None,
-        metadata: Optional[Variant] | None,
-    ) -> None:
-        """Update song artist and title, including playback status"""
-
-        if metadata is None:
-            pass
-        else:
-            artist = metadata.value["xesam:artist"].value[0]
-            song = metadata.value["xesam:title"].value
-            self.now_playing = f"{artist} - {song}"
-            self.now_playing.replace("\n", "")
-
-            if len(self.now_playing) > 35:
-                self.now_playing = self.now_playing[:35]
-                self.now_playing += "…"
-                if "(" in self.now_playing and ")" not in self.now_playing:
-                    self.now_playing += ")"
-
-            self.now_playing = self.now_playing.replace("&", "&amp;")
-
-        if playbackstatus == "Paused":
-            self.playback_icon = f"<span foreground='{colors['primary']}'> \
-                                </span>"
-
-        elif playbackstatus == "Playing":
-            self.playback_icon = f"<span foreground='{colors['primary']}'> \
-                                契</span>"
-        self.qtile.call_soon(self.bar.draw)
-        self.text = f"{self.playback_icon} {self.now_playing}"
